f6da735db062c1e4a2d836702432e8851c4e6158
Kevin Cooper <cooperka92@gmail.com>
2016-10-10 09:02:24 -0400

    Convert regular ListView to ImmutableListView

--- example/src/App.js
+++ example/src/App.js
@@ -1,25 +1,11 @@
 import React, { Component } from 'react';
-import { Text, View, ListView } from 'react-native';
+import { Text, View } from 'react-native';
+import ImmutableListView from 'react-native-immutable-list-view';
 
 import style from './styles';
 import listData from './listData';
 
 class App extends Component {
-
-  constructor(props) {
-    super(props);
-
-    const dataSource = new ListView.DataSource({
-      rowHasChanged: (r1, r2) => r1 !== r2,
-      sectionHeaderHasChanged: (s1, s2) => s1 !== s2,
-    });
-
-    const mutableData = listData.toJS();
-
-    this.state = {
-      dataSource: dataSource.cloneWithRowsAndSections(mutableData),
-    };
-  }
 
   renderRow(rowData) {
     return <Text style={style.row}>{rowData}</Text>;
@@ -35,8 +21,8 @@ class App extends Component {
         <Text style={style.welcome}>
           Welcome to React Native!
         </Text>
-        <ListView
-          dataSource={this.state.dataSource}
+        <ImmutableListView
+          immutableData={listData}
           renderRow={this.renderRow}
           renderSectionHeader={this.renderSectionHeader}
         />
