20a9513f1c4265497b21aaaf491bfb2ffb214312
Kevin Cooper <cooperka92@gmail.com>
2016-10-29 18:20:52 -0400

    Let example app use actual source rather than published module

--- example/src/App.js
+++ example/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
-import ImmutableListView from 'react-native-immutable-list-view';
+import ImmutableListView from 'react-native-immutable-list-view/src/ImmutableListView';
 
 import style from './styles';
 import listData from './listData';
