a409583603c4f61bdc07037f89775c3bbedb0d60
Kevin Cooper <cooperka92@gmail.com>
2017-03-15 11:36:28 -0400

    :white_check_mark: Add tests for EmptyListView

---
+++ src/__tests__/EmptyListView.test.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import { renderers } from './testUtils';
+
+import EmptyListView from '../EmptyListView';
+
+describe('EmptyListView', () => {
+  it('renders with default text', () => {
+    const tree = renderer.create(
+      <EmptyListView />,
+    );
+    expect(tree.toJSON()).toMatchSnapshot();
+  });
+
+  it('renders with custom text', () => {
+    const tree = renderer.create(
+      <EmptyListView
+        emptyText="Nothing. Nothing at all."
+      />,
+    );
+    expect(tree.toJSON()).toMatchSnapshot();
+  });
+
+  it('renders with custom renderRow', () => {
+    const tree = renderer.create(
+      <EmptyListView
+        emptyText="Nothing. Nothing at all."
+        renderRow={() => renderers.renderRow('Overridden!')}
+      />,
+    );
+    expect(tree.toJSON()).toMatchSnapshot();
+  });
+});
