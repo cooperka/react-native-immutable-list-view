23b7264bac490ccdf45e556c951feefcb3ce0225
Kevin Cooper <cooperka92@gmail.com>
2017-03-27 21:24:03 -0400

    :rocket: Update download-flatlist.sh with new file locations

--- bin/download-flatlist.sh
+++ bin/download-flatlist.sh
@@ -3,14 +3,16 @@
 # Based on https://gist.github.com/cooperka/c5dd3ab11f588044d4d6ba22d52c4ab0
 # Downloads FlatList and its related dependencies directly into your node_modules.
 
-FILES=('FlatList' 'MetroListView' 'SectionList' 'VirtualizedList' 'ViewabilityHelper' 'VirtualizeUtils' 'VirtualizedSectionList')
-OLD_DIR='node_modules/react-native/Libraries/Experimental'
-NEW_DIR='node_modules/react-native/Libraries/CustomComponents/Lists'
+FILES=('FlatList' 'MetroListView' 'SectionList' 'VirtualizedList' 'VirtualizedSectionList' 'ViewabilityHelper' 'VirtualizeUtils')
+OLD_DIR_1='node_modules/react-native/Libraries/Experimental'
+OLD_DIR_2='node_modules/react-native/Libraries/CustomComponents/Lists'
+NEW_DIR='node_modules/react-native/Libraries/Lists'
 
 mkdir -p ${NEW_DIR} && \
 for file in ${FILES[@]}; \
   do \
     echo $file; \
-    rm -f ${OLD_DIR}/${file}.js; \
-    curl https://raw.githubusercontent.com/facebook/react-native/master/Libraries/CustomComponents/Lists/${file}.js > ${NEW_DIR}/${file}.js; \
+    rm -f ${OLD_DIR_1}/${file}.js; \
+    rm -f ${OLD_DIR_2}/${file}.js; \
+    curl https://raw.githubusercontent.com/facebook/react-native/master/Libraries/Lists/${file}.js > ${NEW_DIR}/${file}.js; \
   done
