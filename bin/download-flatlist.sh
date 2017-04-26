56702d976f7ea7ae92557c45d322309e7d5b1976
Kevin Cooper <cooperka92@gmail.com>
2017-04-26 13:50:56 -0400

    :green_heart: Add 'FillRateHelper' to download-flatlist; commit snapshots

--- bin/download-flatlist.sh
+++ bin/download-flatlist.sh
@@ -3,7 +3,7 @@
 # Based on https://gist.github.com/cooperka/c5dd3ab11f588044d4d6ba22d52c4ab0
 # Downloads FlatList and its related dependencies directly into your node_modules.
 
-FILES=('FlatList' 'MetroListView' 'SectionList' 'VirtualizedList' 'VirtualizedSectionList' 'ViewabilityHelper' 'VirtualizeUtils')
+FILES=('FlatList' 'MetroListView' 'SectionList' 'VirtualizedList' 'VirtualizedSectionList' 'ViewabilityHelper' 'VirtualizeUtils' 'FillRateHelper')
 OLD_DIR_1='node_modules/react-native/Libraries/Experimental'
 OLD_DIR_2='node_modules/react-native/Libraries/CustomComponents/Lists'
 NEW_DIR='node_modules/react-native/Libraries/Lists'
