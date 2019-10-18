b56989364e85e8325fa4c4d6ecca99153520e364
Kevin Cooper <cooperka92@gmail.com>
2019-10-18 16:40:52 -0700

    :fire: Temporarily remove old example app

--- example/index.js
+++
@@ -1,7 +0,0 @@
-import { AppRegistry } from 'react-native';
-
-// Choose one:
-import App from './src/ImmutableListViewExample';
-// import App from './src/ImmutableVirtualizedListExample';
-
-AppRegistry.registerComponent('ImmutableListViewExample', () => App);
